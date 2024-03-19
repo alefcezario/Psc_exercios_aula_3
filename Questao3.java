c2cbfa336722aad165e4387661857d46b039c0de
Alef Cezario <161400415+alefcezario@users.noreply.github.com>
2024-03-18 21:00:58 -0300

    Create Questao3.java
    3. Escreva um programa em Java para ler o número total de eleitores de um município, o número de votos brancos, nulos e válidos. Calcular e escrever o percentual que cada um representa em relação ao total de eleitores.

---
+++ Questao3.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Questao3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+      
+      double votosBrancos, votosNulos, votosValidos, totalEleitores;
+      double percentualBrancos, percentualNulos, percentualValidos;
+      
+       System.out.println("Digite a quantidade de eleitores que votaram em branco:");
+       votosBrancos = scanner.nextDouble();
+       
+       System.out.println("Agora digite a quantidade de eleitores que votaram nulo:");
+       votosNulos = scanner.nextDouble();
+       
+       System.out.println("E por ultimo a quantidade de votos válidos:");
+       votosValidos = scanner.nextDouble();
+       
+       totalEleitores = votosBrancos + votosNulos + votosValidos;
+       percentualBrancos = votosBrancos/totalEleitores*100;
+       percentualNulos = votosNulos/totalEleitores*100;
+       percentualValidos = votosValidos/totalEleitores*100;
+       
+       System.out.println(percentualBrancos+"% dos eleitores votaram em branco.");
+       System.out.println(percentualNulos+"% dos eleitores anularam seu voto.");
+       System.out.println(percentualValidos+"% dos votos foram validos!");
+              
+       scanner.close();     
+    }
+
+}
